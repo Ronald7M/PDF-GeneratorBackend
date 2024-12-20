35aa6ece5d06679b04bb71fdd91a9bdece0b003a
Ronald7M <ronald.moldovan@yahoo.com>
2024-12-20 15:24:47 +0200

    Add new feature

--- configDataBase.js
+++ configDataBase.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('mydatabase',  process.env.DB_USER,  process.env.DB_PASS, {
-    host: 'localhost',
-    port: '3307',
+    host: 'chacha-db',
+    port: '3306',
     dialect: 'mysql',
 });
 
