64017da405c2712cdfda2a86ad283c03292ae4df
Ronald7M <133379652+Ronald7M@users.noreply.github.com>
2025-01-11 15:52:32 +0200

    Update configDataBase.js

--- configDataBase.js
+++ configDataBase.js
@@ -1,16 +1,16 @@
 const { Sequelize } = require('sequelize');
 
-// const sequelize = new Sequelize('mydatabase',  process.env.DB_USER,  process.env.DB_PASS, {
-//     host: 'chacha-db',
-//     port: '3306',
-//     dialect: 'mysql',
-// });
-
-const sequelize = new Sequelize('mydatabase',  'chahca',  'barbershop', {
-    host: 'ronsky.ro',
-    port: '3307',
+const sequelize = new Sequelize('mydatabase',  process.env.DB_USER,  process.env.DB_PASS, {
+    host: 'chacha-db',
+    port: '3306',
     dialect: 'mysql',
 });
+
+// const sequelize = new Sequelize('mydatabase',  'chahca',  'barbershop', {
+//     host: 'ronsky.ro',
+//     port: '3307',
+//     dialect: 'mysql',
+// });
 
 console.log("da")
 
